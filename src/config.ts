53c13a2ab327c385f4c24a7aee032e241b4ab5f4
mychatterbox <mychatterbox@gmail.com>
2024-02-16 00:31:03 +0900

    header

--- src/config.ts
+++ src/config.ts
@@ -2,9 +2,9 @@ import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
   website: "https://chatter.kr/", // replace this with your deployed domain
-  author: "Sat Naing",
+  author: "mychatterbox",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "mychatterbox",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
